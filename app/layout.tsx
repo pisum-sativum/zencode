f210a9680f5a95cd0b88bdd4224af4ef195cc928
pisum-sativum <ankitajha102@gmail.com>
2026-04-16 19:12:00 +0530

    fix(ui): remove stray whitespace node in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,6 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-full flex flex-col">
-        {" "}
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
